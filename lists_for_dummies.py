66f0a172d0617eda90f80e7150a9bd68d86ca621
ismahenelarbi <meriemismahlarbi@gmail.com>
2022-03-27 19:41:01 +0200

    list comprehension: computing cartesian product

--- lists_for_dummies.py
+++ lists_for_dummies.py
@@ -20,3 +20,9 @@ print(squared_even_numbers)
 #comprehension
 squared_even_numbers = [num ** 2 for num in nums if (num ** 2) % 2 == 0]
 print(squared_even_numbers)
+
+# computing cartesian product:
+A = [1, 3, 5, 7]
+B = [2, 4, 6, 8]
+
+cartesian_product = [(a, b) for a in A and b in B]
